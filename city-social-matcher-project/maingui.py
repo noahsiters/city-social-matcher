72998b0bac726e69bf134aa00cc5fb60d1d7800a
noahsiters <noahsiters@gmail.com>
2024-06-26 01:11:48 -0400

    - replaced tkinter buttons with customtkinter buttons  - added new settings window  - added feature to update API key

--- city-social-matcher-project/maingui.py
+++ city-social-matcher-project/maingui.py
@@ -1,13 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 
+import customtkinter
 import matcher
+import settings
+
+root = Tk()
 
 def createMainWindow(*args):
 
     # set up main application window
-    root = Tk()
     root.title("City Social Matcher")
+    # root.geometry("500x500")
 
     # set up content frame
     mainframe = ttk.Frame(root, padding="6 6 12 12")
@@ -20,7 +24,7 @@ def createMainWindow(*args):
     formId = StringVar()
     formId_entry = ttk.Entry(mainframe, width=20, textvariable=formId)
     formId_entry.grid(column=2, row=1, sticky=(W, E))
-    ttk.Label(mainframe, text="Jotform Form ID: ").grid(column=1, row=1)
+    customtkinter.CTkLabel(mainframe, text="Jotform Form ID: ").grid(column=1, row=1)
 
     # output
     # matches
@@ -29,9 +33,11 @@ def createMainWindow(*args):
 
     # buttons
     # process button
-    ttk.Button(mainframe, text="Get Matches", command=lambda: matcher.getMatches()).grid(column=3, row=3, sticky=W)
+    customtkinter.CTkButton(mainframe, text="Get Matches", command=lambda: matcher.getMatches()).grid(column=3, row=1, sticky=W)
 
-    ttk.Button(mainframe, text="Get Responses", command=lambda: matcher.getSubmissions(formId.get())).grid(column=2, row=3, sticky=(W, E))
+    customtkinter.CTkButton(mainframe, text="(test) Get Responses", command=lambda: matcher.getSubmissions(formId.get())).grid(column=1, row=3, sticky=(W, E))
+
+    customtkinter.CTkButton(mainframe, text="Settings", command=openSettingsWindow).grid(column=3, row=3, sticky=(W, E))
 
     for child in mainframe.winfo_children():
         child.grid_configure(padx=5, pady=5)
@@ -40,5 +46,36 @@ def createMainWindow(*args):
     root.bind("<Return>", lambda: matcher.getMatches())
 
     root.mainloop()
+
+def openSettingsWindow(*args):
+     
+    # Toplevel object which will 
+    # be treated as a new window
+    settingsWindow = Toplevel(root)
+ 
+    # sets the title of the
+    # Toplevel widget
+    settingsWindow.title("Settings")
+ 
+    # # sets the geometry of toplevel
+    # settingsWindow.geometry("300x300")
+
+    # set up content frame
+    # settingsFrame = ttk.Frame(root, padding="6 6 12 12")
+    # settingsFrame.grid(column=0, row=0, sticky=(N, S, E, W))
+    # root.columnconfigure(0, weight=1)
+    # root.rowconfigure(0, weight=1)
+ 
+    # A Label widget to show in toplevel
+    # Label(settingsWindow, 
+    #       text ="This is a new window").pack()
+
+    for child in settingsWindow.winfo_children():
+        child.grid_configure(padx=5, pady=5)
+    
+    apikey = StringVar()
+    apikey_entry = ttk.Entry(settingsWindow, width=20, textvariable=apikey)
+    apikey_entry.grid(column=1, row=1, sticky=(W, E))
+    ttk.Button(settingsWindow, text="Update API Key", command=lambda: settings.updateAPIKey(apikey.get())).grid(column=3, row=1, sticky=W)
 
 # createMainWindow()
